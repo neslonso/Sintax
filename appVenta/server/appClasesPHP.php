034ffaefb903b8e694b67a5c476df1e6e79f6dc6
Néstor Alonso <neslonso@gmail.com>
2016-04-19 12:18:58 +0200

    [-] Corregido bug en autocarga de clases (localizaba pedBridge.php en lugar de Bridge.php por error en regex de búsqueda de ficheros)

--- appVenta/server/appClasesPHP.php
+++ appVenta/server/appClasesPHP.php
@@ -1,7 +1,7 @@
 <?
 spl_autoload_register(function ($clase) {
 	$clase = @end(explode('\\',$clase));//namespaces, genera: PHP Strict Standards:  Only variables should be passed by reference, pq end recibe el resultado de explode por referencia
-	$fileList=Filesystem::folderSearch(dirname(__FILE__).DIRECTORY_SEPARATOR.'clases'.DIRECTORY_SEPARATOR,'/.*'.$clase.'.php$/');
+	$fileList=Filesystem::folderSearch(dirname(__FILE__).DIRECTORY_SEPARATOR.'clases'.DIRECTORY_SEPARATOR,'/.*\/'.$clase.'.php$/');
 	foreach ($fileList as $filePath) {
 		if (file_exists($filePath)) {
 			require_once($filePath);
@@ -9,7 +9,7 @@ spl_autoload_register(function ($clase) {
 	}
 	$fileList=Filesystem::folderSearch(dirname(__FILE__).DIRECTORY_SEPARATOR.
 		'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'appZzShared'.DIRECTORY_SEPARATOR.'server'.DIRECTORY_SEPARATOR.'clases'.DIRECTORY_SEPARATOR,
-		'/.*'.$clase.'.php$/');
+		'/.*\/'.$clase.'.php$/');
 	foreach ($fileList as $filePath) {
 		if (file_exists($filePath)) {
 			require_once($filePath);
