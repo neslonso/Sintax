176add41e4cb9f866738d0135d68b88db9a817d7
luciaroper <lucia@jacaba.com>
2016-09-16 14:54:42 +0200

    [*]

---
+++ appFed16/server/clases/Pages/StagePlugin/markup/js.php
@@ -0,0 +1,6 @@
+<?if (false) {?><script><?}?>
+<?="\n/*".get_class()."*/\n"?>
+$(document).ready(function() {
+	//$('#divJqCesta').jqNotification({'foo': 'bar'});
+	$('#divJqNotifications').jqNotifications({'foo': 'bar'});
+});
---
+++ appFed16/server/clases/Pages/StagePlugin/markup/markup.php
@@ -0,0 +1,14 @@
+<?="\n<!-- ".get_class()." -->\n"?>
+<?="\n<!-- /".get_class()." -->\n"?>
+<nav>
+<header id="">
+	<div id="divJqCesta"></div>
+</header>
+</nav>
+<div id="wrapper" class="toggled">
+
+</div>
+<footer>
+	<div id="divJqNotifications"></div>
+</footer>
+<!-- /#wrapper -->
