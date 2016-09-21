b01f3eac3c04e6716668383e042d76b0d5121406
Néstor Alonso <neslonso@gmail.com>
2016-09-21 14:41:58 +0200

    [+] Preparación para atender múltiples dominios a través de proxy [+] Función de llamadas a apiLocal [-] Bugs en Entity [*] modulo api devuelve resultados (tiene que seguir evolucionando, hasta alcanzar una mecánica análogo a la del modulo render, resultado=servicio(usuario))

--- appZzShared/server/sharedDefines.php
+++ appZzShared/server/sharedDefines.php
@@ -1,5 +1,48 @@
 <?
 /**/
+define ('ARR_DOMAINS',serialize(array (
+	'celorriofarma.com' => (object) array('keyTienda' => 'CL','session_name' => 'clPHPSESSID',),
+	'celorriofarma.es' => (object) array('keyTienda' => 'CL','session_name' => 'clPHPSESSID',),
+	'subastafarma.com' => (object) array('keyTienda' => 'CL','session_name' => 'clPHPSESSID',),
+	'farmadiscount.com' => (object) array('keyTienda' => 'FD','session_name' => 'fdPHPSESSID',),
+	'parafarmaciasolobebes.com' => (object) array('keyTienda' => 'SB','session_name' => 'sbPHPSESSID',),
+	'psbb.es' => (object) array('keyTienda' => 'SB','session_name' => 'sbPHPSESSID',),
+	'parafarmaciasolocosmetica.com' => (object) array('keyTienda' => 'SC','session_name' => 'scPHPSESSID',),
+	'afarma.es' => (object) array('keyTienda' => 'SC','session_name' => 'scPHPSESSID',),
+	'pscc.es' => (object) array('keyTienda' => 'SC','session_name' => 'scPHPSESSID',),
+	'bebefarma.com' => (object) array('keyTienda' => 'BF','session_name' => 'bfPHPSESSID',),
+	'parafarmaciabebefarma.com' => (object) array('keyTienda' => 'BF','session_name' => 'bfPHPSESSID',),
+	'farmaciacelorrio.com' => (object) array('keyTienda' => 'FC','session_name' => 'fcPHPSESSID',),
+	'comprasenfarmacia.com' => (object) array('keyTienda' => 'FC','session_name' => 'fcPHPSESSID',),
+)));
+
+define ('ARR_TIENDAS',serialize(array (
+	'CL' => array (
+		'SITE_NAME' => '',
+		'FROM_EMAIL' => '',
+	),
+	'FD' => array (
+		'SITE_NAME' => '',
+		'FROM_EMAIL' => '',
+	),
+	'SB' => array (
+		'SITE_NAME' => '',
+		'FROM_EMAIL' => '',
+	),
+	'SC' => array (
+		'SITE_NAME' => '',
+		'FROM_EMAIL' => '',
+	),
+	'BF' => array (
+		'SITE_NAME' => '',
+		'FROM_EMAIL' => '',
+	),
+	'FC' => array (
+		'SITE_NAME' => '',
+		'FROM_EMAIL' => '',
+	),
+)));
+
 define('SITE_NAME','00.skel');
 define('TITLE_BASE',SITE_NAME);
 define('FROM_EMAIL','noreply@'.BASE_DOMAIN);
