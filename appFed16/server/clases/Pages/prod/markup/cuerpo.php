e4ba6aa40a0b1d8bed69750f026f3a855afd942e
luciaroper <lucia@jacaba.com>
2016-10-05 12:31:51 +0200

    [+] markup y php de ficha producto

--- appFed16/server/clases/Pages/prod/markup/cuerpo.php
+++ appFed16/server/clases/Pages/prod/markup/cuerpo.php
@@ -17,7 +17,16 @@
 				<div class="name">
 					<h1 class="h4"><strong><?=$objOferta->GETnombre()?></strong></h1>
 				</div>
-				<p class="text-muted hidden-sm-down m-b-0"><?=$objOferta->GETnombre()?></p>
+				<!--<p class="text-muted hidden-sm-down m-b-0"><?=$objOferta->GETnombre()?></p>-->
+<?
+if($objOferta->GETtipoDevolucionCredito()>0){
+			$rebote = '<div class="shop-item-rebote" data-toggle="tooltip" title="Con la compra de '.$objOferta->GETnombre().' recibirá el '.$objOferta->GETtipoDevolucionCredito().'% de su importe como crédito para futuras compras" data-index="-1"><div>'.$objOferta->GETtipoDevolucionCredito().'%</div></div>';
+?>
+				<?=$rebote?>
+
+<?
+}
+?>
 				<div class="row">
 					<div class="col-xs-12">
 						<div class="">
@@ -38,18 +47,20 @@
 					<div class="col-xs-12 col-sm-9">
 						<span class="text-muted"><?=$objCategoria->nombre?></span>
 					</div>
-					<?
+<?
 					//if producto rebote
-					?>
+	if($objOferta->GETtipoDevolucionCredito()>0){
+
+?>
 					<div class="col-xs-12 col-sm-3">
 						<strong>Crédito:</strong>
 					</div>
 					<div class="col-xs-12 col-sm-9">
-						<span class="text-muted">CREDITO</span>
+						<span class="text-muted">Con esta compra acumulas un <?=$objOferta->GETtipoDevolucionCredito()?>% de su importe como crédito para futuras compras </span>
 					</div>
-					<?
-
-					?>
+<?
+}
+?>
 					<div class="col-xs-12 col-sm-3">
 						<strong>Cantidad:</strong>
 					</div>
@@ -64,30 +75,100 @@
 <?
 	$disponibilidad = ($objOferta->GETagotado()==1) ? 'Agotado' : 'Disponible';
 	$agotado = ($objOferta->GETagotado()==1) ? 'disabled' : '';
+	$classText = ($objOferta->GETagotado()==1) ? 'text-danger' : 'text-success';
 ?>
-						<?=$disponibilidad?>
+						<span class="<?=$classText?>"><?=$disponibilidad?></span>
 					</div>
 				</div>
 			</div>
 			<div class="card-item-white">
 				<div class="row">
-					<div class="col-xs-6">
-						<button type="button" <?=$agotado?> class="jqCst btn btn-default btn-success btnAddCart" data-id="<?=$objOferta->GETid()?>"  data-ttl="<?=$objOferta->GETnombre()?>" data-unit="1" data-prc="<?=$objOferta->pvp()?>" data-src="<?=$objOferta->imgSrc()?>"><i class="glyphicon glyphicon-shopping-cart"></i>&nbsp;Añadir al carrito</button>
+					<div class="col-md-6 hidden-xs">
+						<button type="button" <?=$agotado?> class="jqCst btn btn-default btn-comprar btnAddCart" data-id="<?=$objOferta->GETid()?>"  data-ttl="<?=$objOferta->GETnombre()?>" data-unit="1" data-prc="<?=$objOferta->pvp()?>" data-src="<?=$objOferta->imgSrc()?>"><i class="glyphicon glyphicon-shopping-cart"></i>&nbsp;Añadir al carrito</button>
 					</div>
-					<div class="col-xs-6">
-						<button type="button" <?=$agotado?> class="btnCheckOrder btn btn-default btn-success btnOrder <?=$agotado?>" ><i class="glyphicon glyphicon-ok-circle"></i>&nbsp;Comprar ahora</button>
+					<div class="col-md-6 hidden-xs">
+						<button type="button" <?=$agotado?> class="btn btn-default btn-warning btnOrder <?=$agotado?>" ><i class="glyphicon glyphicon-ok-circle"></i>&nbsp;Comprar ahora</button>
+					</div>
+					<div class="col-xs-6 visible-xs">
+						<button type="button" <?=$agotado?> class="jqCst btn btn-default btn-comprar btnAddCart" data-id="<?=$objOferta->GETid()?>"  data-ttl="<?=$objOferta->GETnombre()?>" data-unit="1" data-prc="<?=$objOferta->pvp()?>" data-src="<?=$objOferta->imgSrc()?>"><i class="glyphicon glyphicon-shopping-cart"></i></button>
+					</div>
+					<div class="col-xs-6 visible-xs">
+						<button type="button" <?=$agotado?> class="btn btn-default btn-warning btnOrder <?=$agotado?>" ><i class="glyphicon glyphicon-ok-circle"></i>&nbsp;</button>
 					</div>
 				</div>
 			</div>
 		</div><!--ofertas-->
 	</div><!--item detail-->
 
-	<div class="row item-detail">
+	<!--swiper articulos relacionados-->
+	<div class="row item-relacionados">
+		<div class="col-lg-12">
+			<div class="card-item-white">
+				<p class="h4 titulo-encabezado">Productos relacionados<small class="text-muted p-l-1">Los usuarios también han comprado</small>
+				</p>
+			</div>
+			<div id="container-banners">
+				<div class="swiper-container">
+					<div class="swiper-wrapper">
+
+			<?
+					foreach ($arrOfertasRelacionadas as $stdObjOfer) {
+			?>
+							<div class="swiper-slide">
+								<?\Sintax\ApiService\Productos::fichaProductoResponsive($stdObjOfer)?>
+							</div>
+			<?
+					}
+			?>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<!--swiper productos gama-->
+	<div class="row item-gama">
+		<div class="col-lg-12">
+			<div class="card-item-white">
+				<p class="h4 titulo-encabezado">Productos gama<small class="text-muted p-l-1"><?=$objOferta->GETnombre()?></small>
+				</p>
+			</div>
+			<div id="container-banners">
+				<div class="swiper-container">
+					<div class="swiper-wrapper">
+
+			<?
+					foreach ($arrOfertasGama as $stdObjOfer) {
+			?>
+							<div class="swiper-slide">
+								<?\Sintax\ApiService\Productos::fichaProductoResponsive($stdObjOfer)?>
+							</div>
+			<?
+					}
+			?>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+
+
+	<div class="row item-desc">
 		<div class="col-xs-12 ">
 			<div class="card-item-white">
-				<div class="desc-item">
-					<h4>Descripción</h4>
-					<p><?=$objOferta->GETdescripcion()?></p>
+				<p class="h4 titulo-encabezado">Descripción<small class="text-muted p-l-1"><?=$objOferta->GETnombre()?></small>
+				</p>
+			</div>
+			<div class="card-item-white">
+				<div class="item-desc-text">
+<?
+	$descripcion = str_replace ( '<br /><br />' , '<br />' , $objOferta->GETdescripcion());
+	//$descripcion = str_replace ( '<br />\n<br />' , '<br />' , $descripcion);
+?>
+					<p><?=trim($objOferta->GETdescripcion())?></p>
 				</div>
 			</div>
 		</div><!--descripcion-->
