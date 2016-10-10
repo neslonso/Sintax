5dc29178c4e92336d98ea088b3b0ee7f5d0dba13
luciaroper <lucia@jacaba.com>
2016-10-10 09:09:02 +0200

    [*] centrado iconos notificaciones

--- appFed16/server/clases/Pages/Home/markup/css.php
+++ appFed16/server/clases/Pages/Home/markup/css.php
@@ -1,8 +1,11 @@
 <?if (false) {?><style><?}?>
 <?="\n/*".get_class()."*/\n"?>
 /*********** DEFINICION DE VARIABLES ************/
-@color-principal: #CD5A91; /*#ff94be*/
-@color-secundario: #6c94be;
+/* https://color.adobe.com/es/create/color-wheel/ */
+@color-principal: <?=$GLOBALS['config']->tienda->TEMA->COLOR_PRIMARIO?>; /*#ff94be*/
+@color-secundario: <?=$GLOBALS['config']->tienda->TEMA->COLOR_SECUNDARIO?>;
+/*@color-terciario: spin(@color-principal, 210%);*/
+@color-terciario: multiply(@color-principal, @color-secundario);
 @color-links: #304c71;
 @color-links-hover: #6c94be;
 
@@ -13,6 +16,8 @@
 @color-cuerpo: #ebeced;
 @color-sidebarMenu: #fff;
 @color-pie: #fff;
+
+@color-banner-txt: @color-terciario;
 
 @color-borde-items: #a0a0a0;
 @color-bg-items: #fff;
@@ -32,6 +37,13 @@
 
 @height-banner: 250px;
 
+/**/
+/* http://stackoverflow.com/questions/19288546/how-can-i-prevent-body-scrollbar-and-shifting-when-twitter-bootstrap-modal-dialo */
+.modal-open {
+	overflow: auto;
+	padding-right: 0px !important;
+}
+/**/
 /*********** ZONAS GENERALES ************/
 body {
 	background-color: @color-body;
@@ -78,20 +90,14 @@ a.blanco{
 }
 #container-pie {
 	background-color: @color-pie;
+	box-shadow: 0 -3px 5px 0 rgba(0, 0, 0, 0.25);
+	margin-top: 10px;
+}
+#container-pie .img-responsive {
+	margin:auto;
 }
 #container-pie img{
 	margin: 10px;
-}
-.container-menu{
-	padding-left: 0px !important;
-	padding-right: 0px !important;
-}
-.rootMenu{
-	padding-left: 5px !important;
-	/*
-	background-repeat: no-repeat !important;
-	background-position: -1px center !important;
-	*/
 }
 /*
 .nav > li > a{
@@ -101,140 +107,7 @@ a.blanco{
 .navbar-default .navbar-nav > li > a {
 	padding-top: 15px !important;
 }
-.nav-user-menu{
-    position: absolute;
-    top: 116px ;
-    right: -300px !important;
-    width: 300px !important;
-    height: auto !important;
-    border-top: 3px solid @color-principal;
-    overflow: hidden;
-    box-shadow: 0 5px 15px -3px rgba(0, 0, 0, 0.23);
-    background-color: #fff;
-    padding: 10px;
-}
- .nav-user-panel{
- 	margin-bottom: 0px;
- }
-.nav-user-avatar{
-    font-size: 43px;
-}
-.nav-user-menu-credito{
-    background-image: url('<?=BASE_DIR?>index.php?MODULE=images&almacen=IMGS_DIR&filtro=grayscale&fichero=tarjetaAnverso.png&ancho=75');
-    background-position: center;
-    background-repeat: no-repeat;
-    padding-top: 24px;
-    padding-bottom: 10px;
-    text-align: center;
-    color: #fff;
-    height: 52px;
-    font-size: 17px;
-}
-.nav-user-menu-descuento{
-    padding-top: 11px;
-    font-size: 20px;
-}
-.nav-user-menu-descuento span{
-    color: green;
-}
- .nav-user-txt-info{
- 	font-size: 12px;
- 	font-weight: normal;
- 	padding-top: 20px;
- }
-.nav-user-menu-logunt{
-    padding-top: 10px;
-}
-/*estilos desplazamiento menu user*/
-.nav-user-menu-inside {
-  animation: lateralMoveInMenu 0.5s;
-  animation-direction: alternate;
-  animation-fill-mode: forwards;
-  animation-timing-function: ease-in;
-}
-.nav-user-menu-outside {
-  animation: lateralMoveOutMenu 0.5s;
-  animation-direction: alternate-reverse;
-  animation-timing-function: ease-in;
-}
-@keyframes lateralMoveInMenu {
-  0% {
-    transform: translate(0, 0);
-  }
-  100% {
-    -ms-transform: translateX(-300px); /* IE 9 */
-    -webkit-transform: translateX(-300px); /* Chrome, Safari, Opera */
-    transform: translateX(-300px);
-  }
-}
-@keyframes lateralMoveOutMenu {
-  0% {
-    -ms-transform: translateX(0); /* IE 9 */
-    -webkit-transform: translateX(0); /* Chrome, Safari, Opera */
-    transform: translateX(0);
-  }
-  100% {
-    -ms-transform: translateX(-300px); /* IE 9 */
-    -webkit-transform: translateX(-300px); /* Chrome, Safari, Opera */
-    transform: translateX(-300px);
-  }
-}
-.txtMenuRoot{
-	display: inline-block;
-	/*vertical-align: middle;
-	vertical-align: -moz-middle-with-baseline;
-	line-height: normal;*/
-	width: @width-root-txt-menu;
-	float:left;
-	padding-left: 5px;
-	height:30px;
-	line-height: 30px;
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	text-transform: capitalize;
-	font-variant: small-caps;
-	font-size: smaller;
-}
-.rootMenu-ico{
-	width: 30px;
-	height: 30px;
-	display: inline-block;
-	float: left;
-	border: solid @color-principal 1px;
-	border-radius: 3px;
-	background-color: #fff;
-}
-.col-subMenu{
-	-moz-column-count: 3;
-	-webkit-column-count: 3;
-	column-count: 3;
-}
-.col-subMenu .containerImgDecoCat {
-	padding-top: 13px;
-}
-.img-cat-subMenu{
-	width: 30px;
-	height: 30px;
-	display: inline-block;
-	float: left !important;
-	/*margin-top: 0px !important;*/
-	/*margin-bottom: -10px !important;*/
-	margin-right: 3px;
-}
-.nombre-cat-subMenu{
-	float: left !important;
-}
-.ttmenu-content .box li {
-	padding: 5px 5px 5px 0 !important;
-}
 
-.ttmenu-content .box+.child h4 {
-	/*line-height: 30px;*/
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-}
 /*
  * Row with equal height columns
  * --------------------------------------------------
@@ -261,45 +134,7 @@ a.blanco{
 	justify-content: center;	/* Optional, to align inner flex-items
 					horizontally within the column  */
 }
-
-
-/******** buscador ********/
-#custom-search-input {
-	margin:0;
-	padding: 0;
-	width: 100%
-}
-#custom-search-input .search-query {
-	padding-right: 3px;
-	padding-right: 4px \9;
-	padding-left: 3px;
-	padding-left: 4px \9;
-	/* IE7-8 doesn't have border-radius, so don't indent the padding */
-	margin-bottom: 0;
-	-webkit-border-radius: 3px;
-	-moz-border-radius: 3px;
-	border-radius: 3px;
-}
-#custom-search-input button {
-	border: 0;
-	background: none;
-	/** belows styles are working good */
-	padding: 2px 5px;
-	margin-top: 2px;
-	position: relative;
-	left: -28px;
-	/* IE7-8 doesn't have border-radius, so don't indent the padding */
-	margin-bottom: 0;
-	-webkit-border-radius: 3px;
-	-moz-border-radius: 3px;
-	border-radius: 3px;
-	color:@color-principal;
-}
-.search-query:focus + button {
-	z-index: 3;
-}
-
-/*********** WRAPPER MENU ************/
+/*********** WRAPPER general ************/
 #wrapper {
 	padding-left: @width-menu;
 	-webkit-transition: all 0.5s ease;
@@ -348,282 +183,40 @@ a.blanco{
 	*/
 }
 
-/***********  ZONA DE BANNERS *************/
-#container-banners .swiper-container {
-	width: 100%;
-	height: 100%;
-	padding: 25px 0;
-	box-shadow: inset 0 -7px 3px rgba(0, 0, 0, 0.3);
+.ssSearchContainer {
+	border           : solid rgba(0,0,0,0.9) 1px;
+	background-color : rgba(192,192,192,0.9);
+	box-shadow       : 10px 10px 5px 0px rgba(0,0,0,0.75);
+	padding          : 13px;
+}
 
-	background: linear-gradient(90deg, #ffffff, #6c94be, #ffffff, #cd5a91);
-	background-size: 800% 800%;
-	animation: bannerBackground 120s ease infinite;
+.ssSearchItemContainer {
+	/*padding:7px;*/
 }
-@keyframes bannerBackground {
-	0%{background-position:0% 50%}
-	50%{background-position:100% 50%}
-	100%{background-position:0% 50%}
+.ssSearchItemContainer>div {
+	background-color : rgba(255,255,255,0.9);
+	margin  : 15px 0px 15px 0px;
+	padding : 13px;
+	border  : solid black 1px;
+	height: 180px;
 }
-#container-banners .swiper-slide {
-	width:300px;
-}
-#container-banners .swiper-slide>div {
-	height:100%;
-	background-color: #FFF;
-	border: solid #a0a0a0 1px;
-	border-radius: 7px;
-}
-#container-banners .swiper-slide>div>div {
-	height: 100%;
-}
-/*********** SWIPER DE OFERTAS ************/
-.ofersPageSwiperContainer {
-	width: 100%;
-	height: 100%;
-}
-.ofersPageSwiperContainer .swiper-slide {
-	width:300px;
-	height:100%;
-	/*background-color: #fff;
-	border: solid #a0a0a0 1px;
-	border-radius: 7px;*/
-}
-/*
-.imgAnimatedToSlider {
-	background-color: #fff;
-	border: solid #a0a0a0 1px;
-	border-radius:7%;
-}
-*/
-/******************************************/
-/*********  LISTADO DE PRODUCTOS **********/
-
-.shop-item {
-	position: relative;
-	max-width: 360px;
-	margin: 15px auto;
-	padding: 5px;
-	text-align: center;
-	border-radius: 4px;
-	overflow: hidden;
-	border:2px solid @color-borde-items;
-	background-color: @color-bg-items;
-	/*max-width: 200px;*/
-}
-.swiper-slide .shop-item-wrapper {
-	height: 100%;
-}
-.swiper-slide .shop-item {
-	margin:0px;
-	height: 100%;
-}
-.shop-item:hover{
-	border:2px solid @color-borde-items-over;
-}
-.shop-item img {
-	width: 100%;
-	max-width: 360px;
-	margin: 0 auto;
-	/*border: 1px solid #eee;*/
-	border-radius: 3px;
-}
-.shop-item .shop-item-dtls h4 {
-	margin-top: 13px;
-	margin-bottom: 10px;
-	text-transform: uppercase;
-}
-.shop-item .shop-item-dtls .shop-item-price {
-	display: block;
-	margin-bottom: 13px;
-	font-size: 24px !important;
-	color: @color-items-precio;
-}
-.shop-item .shop-item-cart {
-	position: absolute;
-	top: 100%;
-	left: 0;
-	width: 100%;
-	padding-bottom:10px;
-	padding-top: 10px;
-	background-color: @color-btn-items;
-	-webkit-transition: all 0.35s ease-in;
-	-moz-transition: all 0.35s ease-in;
-	-ms-transition: all 0.35s ease-in;
-	-o-transition: all 0.35s ease-in;
-	transition: all 0.35s ease-in;
-}
-.shop-item:hover .shop-item-cart {
-	margin-top: -50px;
-}
-.shop-item .shop-item-cart  a.btn{
-	border:1px solid @color-btn-items-border;
-	color:@color-btn-items-bg-font !important;
-	background:@color-btn-items-bg;
-	-webkit-transition: all 0.35s ease-in;
-	-moz-transition: all 0.35s ease-in;
-	-ms-transition: all 0.35s ease-in;
-	-o-transition: all 0.35s ease-in;
-	transition: all 0.35s ease-in;
-}
-.shop-item-link{
-	cursor: pointer;
-
-}
-.shop-item-name{
+.ssSearchItemContainer .nombre {
 	overflow: hidden;
 	white-space: nowrap;
 	text-overflow: ellipsis;
-	font-size: smaller;
+}
 
-}
-.shop-item .shop-item-cart  a.btn:hover{
-	color:#fff !important;
-	background:transparent;
-}
-.shop-item-rebote{
-	background-image: url("./appFed16/binaries/imgs/shop-item-rebote.png");
-	cursor: pointer;
-	font-size: larger;
-	height: 100px;
-	position: absolute;
-	right: -13px;
-	top: -33px;
-	width: 100px;
-	z-index: @zindex-shop-item-rebote;
-}
-.shop-item-modal{
-	padding: 5px;
-	text-align: center;
-	border-radius: 4px;
-	border:2px solid @color-borde-items;
-	background-color: @color-bg-items;
-	max-height: 700px;
+.ssSearchItemContainer .descripcion {
 	overflow: hidden;
-}
-.shop-item-modal h1{
-	font-size: 24px;
-}
-.shop-item-modal-price{
-	color: @color-items-precio;
-	font-size: 18px;
-	font-weight: bold;
-}
-.shop-item-modal-img{
-	display: inline-block !important;
-}
-.rbm_form_cmrce_btn > button {
-	background: @color-principal none repeat scroll 0 0 !important;
-}
-.rbm_sldr_sc_content > a{
-	opacity: 1 !important;
-	visibility: visible !important;
-}
-.rbm_sldr_sc_control .carousel-control{
-	-webkit-transform: none !important;
-	-ms-transform:  none !important;
-	transform:  none !important;
-}
-.rbm_sldr_sc_control .carousel-control > span{
-	-webkit-transform:  none !important;
-	-ms-transform:  none !important;
-	transform:  none !important;
+	height: 100px;
 }
 
-@media(min-width:768px) {
-
-	#wrapper {
-		padding-left: @width-menu;
-		position: relative;
-		z-index: 29;
-	}
-	#wrapper.toggled {
-		padding-left: 0;
-	}
-	#sidebar-wrapper {
-		width: @width-menu;
-		position: absolute;
-	}
-	#wrapper.toggled #sidebar-wrapper {
-		width: 0;
-		/*position: fixed !important;*/
-	}
-	#page-content-wrapper {
-		position: relative;
-	}
-	#wrapper.toggled #page-content-wrapper {
-		position: relative;
-		margin-right: 0;
-	}
+.ssSearchItemContainer .precio {
+	font-size: larger;
 }
 
-/* LG */
-@media (min-width: 1199px){
-
-}
-/* MD */
-@media (min-width: 992px) and (max-width: 1199px){
-	.col-subMenu{
-		-moz-column-count: 2;
-		-webkit-column-count: 2;
-		column-count: 2;
-	}
-	#vertical .navbar-default .navbar-nav li .vertical-menu {
-		max-width: 700px !important;
-	}
-}
-/* SM */
-@media (min-width: 768px) and (max-width: 991px){
-	#vertical .navbar-default .navbar-nav li .vertical-menu {
-		max-width: 500px !important;
-	}
-}
-/* XS */
-@media (max-width: 767px){
-	/**** menu****/
-	#wrapper {
-		padding-left: 0px !important;
-	}
-	#sidebar-wrapper {
-		left: 0px !important;
-		width:100%;
-		margin-left: 0px;
-		height: auto;
-	}
-	.sidebar-wrapper-scroll{
-		position: inherit !important;
-	}
-	.shop-item {
-		position: relative;
-		max-width: 100% !important;
-	}
-}
-
-
-
-/* XS y SM*/
-@media (max-width: 991px){
-	#container-cabecera {
-		border-top: 5px solid @color-principal;
-	}
-	.container-cabecera-barraLogo{
-		margin-top: 0px !important;
-	}
-	body {
-		margin-top: 115px; /* ajuste de cuerpo debido a navbar fixed */
-	}
-	.col-subMenu{
-		-moz-column-count: 1;
-		-webkit-column-count: 1;
-		column-count: 1;
-	}
-    .nav-user-menu{
-        top: 112px !important;
-    }
-}
-
-
-@media (max-width: @shop-item-container-fluid){
-	.shop-item-container {
-		width:auto !important;
-	}
+.highlight {
+	color: @color-principal;
+	font-size:large;
+	font-weight:bold;
 }
