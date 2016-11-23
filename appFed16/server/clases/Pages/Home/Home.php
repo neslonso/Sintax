16af78f88989a2b754ccf45eab5f9e60b94f7cfb
Diego <djreigosa@gmail.com>
2016-11-23 13:37:20 +0100

    [+] prueba

--- appFed16/server/clases/Pages/Home/Home.php
+++ appFed16/server/clases/Pages/Home/Home.php
@@ -350,7 +350,7 @@ class Home extends Error implements IPage {
 			$GLOBALS['firephp']->info($verify_credentials);
 		}
 	}
-
+//.
 	public function acGrabarCliente($email,$pass=""){
 		if (empty($pass)) {$pass=\Cadena::generatePassword();}
 		$result=\Sintax\ApiService\Clientes::acNuevoCliente($email,$pass,$GLOBALS['config']->tienda->key);
